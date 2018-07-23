14a367e52d0f1a4d2ca67b859e3922d0cc3d9c82
ksarapultsev <ksarapultsev2014@gmail.com>
2018-07-23 11:07:54 +0600

    First project MVC
    Add first project ASP.NET MVC - Hello World

---
+++ HelloMVCApplication/HelloMVCApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloMVCApplication.Startup))]
+namespace HelloMVCApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
