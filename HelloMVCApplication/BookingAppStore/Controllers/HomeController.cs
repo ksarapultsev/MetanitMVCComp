fbde455bd3a53d4de187c3f20364511c32be9641
ksarapultsev <ksarapultsev2014@gmail.com>
2018-07-23 17:10:50 +0600

    Add Views Controllers
    Добавление Views, Controllers

--- HelloMVCApplication/BookingAppStore/Controllers/HomeController.cs
+++ HelloMVCApplication/BookingAppStore/Controllers/HomeController.cs
@@ -3,28 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookingAppStore.Models;
 
 namespace BookingAppStore.Controllers
 {
     public class HomeController : Controller
     {
+        // создадим контекст данных
+        BookContext db = new BookContext();
+           
         public ActionResult Index()
         {
+            // получаем из бд все объекты Book
+            IEnumerable<Book> books = db.Books;
+            // передадим все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Books = books;
+            // Возвращаем представление
             return View();
         }
 
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Buy(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.BookId = id;
             return View();
         }
-
-        public ActionResult Contact()
+        
+        [HttpPost]
+        public string Buy(Purchase purchase)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            purchase.Date = DateTime.Now;
+            //добавляем информацию о покупке в базу данных 
+            db.Purchases.Add(purchase);
+            //сохраняем в БД все изменения
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
         }
+    
     }
 }
---
+++ HelloMVCApplication/BookingAppStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingAppStore.Models
+{
+    public class Book
+    {
+        //Id книги
+        public int Id { get; set; }
+        // название книги
+        public string Name { get; set; }
+        //автор книги
+        public string Author { get; set; }
+        // цена
+        public int Price { get; set; }
+
+
+    }
+}
